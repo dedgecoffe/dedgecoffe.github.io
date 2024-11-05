ed2b26d9f2eb27de61235cf66cdf610b6ea196a6
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2024-11-05 07:58:09 +0700

    style

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
-  <body>
-  <nav class="navbar navbar-expand navbar-dark bg-primary">
+  <body style="height: 3000px;">
+  <nav class="navbar navbar-expand navbar-dark bg-primary sticky-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><i class="bi bi-cup-hot-fill"></i>DeCafe</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
