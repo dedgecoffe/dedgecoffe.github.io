807ccd9daae2afde9c73c0dc9be12874534cc989
Muhammad Malik Nur <Kamalputra1177@gmail.com>
2024-11-06 21:42:16 +0700

    +

--- Mall/index.php
+++ Mall/index.php
@@ -7,34 +7,15 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
-<body style="height: 3000px;">
 
-<nav class="navbar navbar-expand navbar-dark bg-primary sticky-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="bi bi-cup-hot-fill"></i> DeCafe</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Username
-          </a>
-          <ul class="dropdown-menu dropdown-menu-end mt-2">
-            <li><a class="dropdown-item" href="#"><i class="bi bi-person-square"></i>Profil</a></li>
-            <li><a class="dropdown-item" href="#"><i class="bi bi-gear"></i>Setting</a></li>
-            <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-in-left"></i>Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<body style="height: 3000px;">
+    <!-- Header -->
+    <?php include "Header.php"; ?>
+    <!-- End Header -->
 
 <div class="container-lg">
   <div class="row">
-    <!-- Sidebar Menu -->
+    <!-- Sidebar -->
     <?php include "Sidebar.php"; ?>
     <!-- End Sidebar -->
 
